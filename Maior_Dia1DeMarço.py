62f03f670aac8b603d3a15b3e845b26256ca504c
Welliton da Silva Gomes <137785960+WellitonGomes@users.noreply.github.com>
2024-04-19 11:15:33 -0300

    Add files via upload

---
+++ Maior_Dia1DeMarço.py
@@ -0,0 +1,23 @@
+a = input("Digitei um numero inteiro 'A' ")
+#entra com um numero inteiro para a
+#variavel a
+b = input("Digitei um numero inteiro 'B' ")
+#entra com um numero inteiro para b
+#variavel b
+if a > b:
+#se a for maior que b então imprime a
+#linha seguinte
+    print("A é maior que B")
+#mostra a mensagem que a é maior que b
+elif b > a:
+#se B for maior que A mostra a seguinte mensagem
+    print("B é igual a A")
+#imprime a mensagem que B é igual á A
+elif b == a:
+#se B for igual a A mostra a seguinte mensagem
+    print("B é igual a A")
+#mostra a mensagem que B é igual a A
+else:
+#se nada do que foi setado acima ocorre mostra a seguinte mensagem
+    print("Ocorreu algum erro")
+#mostra a mensagem que ocorreu algum erro 
